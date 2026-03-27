0beb7e52aa793f61d58f64a0848d7ab56013b017
Ellie-Cunningham <elliecunningham627@gmail.com>
2026-03-27 16:45:01 -0400

    Statement + decision coverage complete.

--- Backend/tests/test_transfer.py
+++ Backend/tests/test_transfer.py
@@ -2,32 +2,35 @@ from transactions import transfer
 from lists import AccountsList
 import pytest
 
-def test_W1_account_not_found(accounts_list, capsys):
-    transaction = {'code': '02', 'accountName': 'Hugh Mann', 'accountNumber': '99999', 'money': 50, 'misc': ''}
+def test_T1_TO_account_not_found(accounts_list, capsys):
+    transaction = {'code': '01', 'accountName': 'John Doe', 'accountNumber': '99999', 'money': 50, 'misc': ''}
     assert transfer(transaction, accounts_list) is False
     assert f"ERROR: Transfer failed – TO account {transaction['accountNumber']} not found." in capsys.readouterr().out
 
-def test_W2_account_disabled(accounts_list, capsys):
-    transaction = {'code': '02', 'accountName': 'Alice Williams', 'accountNumber': '00004', 'money': 50, 'misc': ''}
+def test_T2_TO_account_disabled(accounts_list, capsys):
+    transaction = {'code': '01', 'accountName': 'John Doe', 'accountNumber': '00004', 'money': 50, 'misc': ''}
     assert transfer(transaction, accounts_list) is False
     assert f"ERROR: Transfer failed – TO account {transaction['accountNumber']} is disabled." in capsys.readouterr().out
 
-def test_W3_insufficient_balance(accounts_list, capsys):
-    transaction = {'code': '02', 'accountName': 'Bob Johnson', 'accountNumber': '00001', 'money': 750, 'misc': ''}
+def test_T3_FROM_account_name_not_found(accounts_list, capsys):
+    transaction = {'code': '01', 'accountName': 'Ghost User', 'accountNumber': '00002', 'money': 50, 'misc': ''}
+    assert transfer(transaction, accounts_list) is False
+    assert f"ERROR: Transfer failed – no active account found for holder '{transaction['accountName']}' to transfer from." in capsys.readouterr().out
+
+def test_T4_name_matches_are_disabled(accounts_list, capsys):
+    transaction = {'code': '01', 'accountName': 'Alice Williams', 'accountNumber': '00002', 'money': 50, 'misc': ''}
+    assert transfer(transaction, accounts_list) is False
+    assert f"ERROR: Transfer failed – no active account found for holder '{transaction['accountName']}' to transfer from." in capsys.readouterr().out
+
+def test_T5_FROM_insufficient_balance(accounts_list, capsys):
+    transaction = {'code': '01', 'accountName': 'Bob Johnson', 'accountNumber': '00002', 'money': 750, 'misc': ''}
     assert transfer(transaction, accounts_list) is False
     assert f"ERROR: Transfer of $750.00 from account 00003 would cause a negative balance – transaction skipped." in capsys.readouterr().out
 
-def test_W4_boundary_exact_depletion(accounts_list):
-    transaction = {'code': '02', 'accountName': 'Bob Johnson', 'accountNumber': '00001', 'money': 749.90, 'misc': ''}
+def test_T6_boundary_exact_from_depletion(accounts_list):
+    transaction = {'code': '01', 'accountName': 'Bob Johnson', 'accountNumber': '00002', 'money': 749.90, 'misc': ''}
     assert transfer(transaction, accounts_list) is True
-    assert accounts_list.current_accounts[2]["balance"] == 0.00
 
-def test_W5_successful_withdrawal_np(accounts_list):
-    transaction = {'code': '02', 'accountName': 'John Doe', 'accountNumber': '00002', 'money': 200, 'misc': ''}
+def test_T7_successful_transfer_np(accounts_list):
+    transaction = {'code': '01', 'accountName': 'John Doe', 'accountNumber': '00002', 'money': 200, 'misc': ''}
     assert transfer(transaction, accounts_list) is True
-    assert accounts_list.current_accounts[0]["balance"] == 799.90
-
-def test_W6_successful_withdrawal_sp(accounts_list):
-    transaction = {'code': '02', 'accountName': 'Charlie Brown', 'accountNumber': '00001', 'money': 50, 'misc': ''}
-    assert transfer(transaction, accounts_list) is True
-    assert accounts_list.current_accounts[4]["balance"] == 4949.95
